fbf14d2087237073b595ec7c117cc715648c594d
Cyphle <cyril.phamle@gmail.com>
2025-08-06 14:43:58 +0200

    add ecr

--- Scaleway/04-applications/01-ecr/main.tf
+++ Scaleway/04-applications/01-ecr/main.tf
@@ -1,6 +1,13 @@
 resource "scaleway_registry_namespace" "banana_back" {
   name        = "banana-back"
   description = "Registry for banana-back, a Node application"
-  is_public   = false   # 🔒 reste privé
+  is_public   = false  
+  region      = "fr-par"
+}
+
+resource "scaleway_registry_namespace" "banana_front" {
+  name        = "banana-front"
+  description = "Registry for banana-front, a React application"
+  is_public   = false   
   region      = "fr-par"
 }
